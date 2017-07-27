6da63a9b2a2b297d7630a5a70c12ff3ba75be968
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-07-27 15:51:25 +0200

    Ajout TP03 et création de la classe TestArray1

---
+++ TP03/src/TestArray1.java
@@ -0,0 +1,13 @@
+
+public class TestArray1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]array={1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4};
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+}
