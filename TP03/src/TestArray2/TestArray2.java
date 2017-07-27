796f7f369782719655eb17cf5d7b30f5450c6b72
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-07-27 16:49:18 +0200

    Ajout TP03 - Ecrire un algorithme pour trouver le nombre d’entiers en doublon dans le tableau

--- TP03/src/TestArray2/TestArray2.java
+++ TP03/src/TestArray2/TestArray2.java
@@ -12,14 +12,33 @@ public class TestArray2 {
 		
 		/*
 		 * Quelle est la moyenne des éléments du tableau ?
-		 */
+		 *
 		int somme = 0;
 		for(int i = 0; i < array.length; i++){
 			somme += array[i];
 		}
 		float moyenne = (float) somme / array.length;
 		System.out.print("\nMoyenne = "+moyenne);
+		*/
 		
+		/*
+		 * Quel est l’index dans le tableau de l’entier 15 ?
+		 * L'entier 15 est en position 1
+		 */
+		
+		
+		/*
+		 * Ecrire un algorithme pour trouver le nombre d’entiers en doublon dans le tableau
+		 */
+		int compteur = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i+1; j < array.length; j++) {
+				if(array[i] == array[j]) {
+					compteur++;
+				}
+			}
+		}
+		System.out.println("compteur = " + compteur);
 
 		
 		
