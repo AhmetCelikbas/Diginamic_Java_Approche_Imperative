6426c3ac80667e2168237f029653d2735b464d4c
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-07-27 16:00:30 +0200

    Ajout TP03 - affichage inversé des elements

--- TP03/src/TestArray1.java
+++ TP03/src/TestArray1.java
@@ -1,13 +1,35 @@
 
+/**
+*
+*@author keylan
+* Classe TestArray1
+*/
 public class TestArray1 {
 
+	/**
+	 * 
+	 * @param args
+	 * Exercice 1 du TP03
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[]array={1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4};
 		
+		/*
+		 * Afficher l’ensemble des éléments du tableau grâce à une boucle :
+		 * 
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
 		}
+		*/
+		
+		/*
+		 * Afficher l’ensemble des éléments dans l’ordre inverse du tableau :
+		 */
+		for (int i = array.length-1; i > 0; i--) {
+			System.out.println(array[i]);
+		}
+		
 	}
 
 }
